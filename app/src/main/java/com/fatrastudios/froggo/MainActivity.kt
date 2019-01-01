5342756d33de50110328356a078787e8864aa79a
Ajka <andreastefkova22@gmail.com>
2019-01-01 19:31:11 +0000

    Added a button to prompt user to make a place to check for permission

--- app/src/main/java/com/fatrastudios/froggo/MainActivity.kt
+++ app/src/main/java/com/fatrastudios/froggo/MainActivity.kt
@@ -2,11 +2,24 @@ package com.fatrastudios.froggo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+     //set up for button click
+    val myBtn = findViewById<Button>(R.id.buttonweather)
+
+        //once the button is clicked
+    myBtn.setOnClickListener {
+        Toast.makeText(this, "Button was clicked", Toast.LENGTH_LONG).show()
+        
+    }
+    }
+
+
 	}
-}
